afd16d903fa625c561e2d44931cd6abcf3081aa4
butadpj <butadpj@gmail.com>
2021-07-11 13:01:44 +0800

    Update deployment script for Github Pages

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:butadpj/simple-ecommerce-app--vuejs-cli.git main:gh-pages
+git push -f https://github.com/butadpj/simple-ecommerce-app--vuejs-cli.git master:gh-pages
 
 cd -
